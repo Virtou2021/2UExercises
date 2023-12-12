97596378bccf8ca88e136cef5ebc717e304a4c4f
Virtou2021 <seba613b@mail.hansenberg.dk>
2023-12-12 12:30:48 +0100

    Exercises

---
+++ Exercises/Exercises.cs
@@ -0,0 +1,24 @@
+//Saying hello and name on different lines
+string Hello = "Hello and welcome back:";
+
+string Name = "Sebastian Gaarde Thomsen";
+
+Console.WriteLine (Hello + "\n" + Name);
+
+//Code to find a sum of two numbers
+int x = 3, y = 7;
+Console.WriteLine(x + " + " + y " = " + (x+y));
+
+//Code to find the result of dividing two numbers
+int x = 6, y = 2;
+Console.WriteLine(x + "/" + y " = " + (x/y));
+
+//Code to solve four math problems
+int a = -1, b = 4, c = 6;
+Console.WriteLine(a + " + " + b + " * " + 6 " = " + (a+b*c));
+int a = 35, b = 5, c = 7;
+Console.Writeline("(" + a + " + " + b + ")" + " % " + c + " = " + ((a+b)%7));
+int a = 14, b = -4, c = 6, d = 11;
+Console.Writeline(a + " + " + b + " * " + c + " / " + d + " = " + (a+b*c/d));
+int a = 2, b = 15, c = 6, d = 1, e = 7, f = 2;
+Console.WriteLine(a + " + " + b + " / " + c + " * " + d + " - " + e + " % " + f + " = " + (a+b/c*d-e%f));
